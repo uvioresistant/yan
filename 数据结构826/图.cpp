bf7a8b0b7bceec59b6e9bc49763444216b6f9083
uvioresistant <kaixin654321@gmail.com>
2020-12-02 01:20:29 +0800

    添加数据结构到'图'结束

--- 数据结构826/图.cpp
+++ 数据结构826/图.cpp
@@ -132,6 +132,109 @@ void BFS_MIN_Distance(Graph G, int u){
 }
 
 
+/*拓扑排序 解释版*/
+#define MaxVertexNum 100		//图中顶点数目的最大值
+typedef struct ArcNode{			//边表结点
+	int adjvex;					//该弧所指向的顶点的位置
+	struct ArcNode *nextarc;	//指向下一条弧的指针
+	InfoType info;				//网的边权值
+}ArcNode;
+
+typedef struct VNode{			//顶点表结点
+	VertexType data;			//顶点信息
+	ArcNode *firstarc;			//指向第一条依附该顶点的弧的指针
+}VNode,AdjList[MaxVertexNum];
+
+typedef struct{
+	AdjList vertices;			//邻接表
+	int vexnum, arcnum;			//图的顶点数和弧数
+}Graph;							//Graph是以邻接表存储的图类型
+
+/*拓扑排序 王道书上的解释*/
+bool TopologicalSort(Graph G){
+	InitStack(S);		//初始化栈，存储入度为0的顶点
+	for(int i=0; i<G.Vexnum; i++)
+		if(indegree[i]==0)	//省略的对indegree数组的声明~记录当前顶点的入度
+			Push(S, i);		// 将所有入度为0的顶点进栈
+	int count=0;		//计数，记录当前已经输出的顶点数
+	while(!IsEmpty(S)){	//栈不空，则存在入度为0的顶点
+		Pop(S,i);		//栈顶元素出栈~每个顶点都需要处理一次
+		print[count++]=i;	//输出顶点i~记录拓扑排序序列
+		for (p=G.vertics[i].firstarc;p;p=p->nextarc){	//将所有i指向的顶点的入度减1，并且将入度减为0的顶点压入栈s
+		v=p->adjvex;	//每条边都需要处理一次
+		if(!(--indegree[v]))
+			Push(S, v);		//入度为0，则入栈
+		}
+		
+	}//while
+	if(count<G.vexnum)
+		return false;		//排序失败，有向图中有回路
+	else
+		return true;		//拓扑排序成功
+	
+}
+
+/*逆拓扑排序的实现(DFS算法)*/
+void DFSTraverse(Graph G){	//对图G进行深度优先遍历
+	for(v=0; v<G.Vexnum; ++v)
+		visited[v]=FALSE;		//初始化已访问标记数据
+	for(v=0; v<G.vexnum; ++v)	//本代码中是从v=0开始遍历
+		if(!visited[v])
+			DFS(G, v);
+}
+
+void DFS(Graph G, int v){		//从顶点v出发，深度优先遍历图G
+	visited[v]=TRUE;			//设已访问标记
+	for(w=FirstNeighbor(G, v); w>=0; w=NextNeighbor(G, v, w))
+		if(!visited[w]){		//w为u的尚未访问的邻接顶点
+			DFS(G,w);
+	}
+	
+	print(v);			//输出顶点
+}
+
+//DFS算法实现有向无环图拓扑排序
+bool visited[MAX_VERTEX_NUM];	//访问标记数组
+void DFSTraverse(Graph G) {
+	//对图G进行遍历，访问函数为visit()
+	for (v=0; v<g.vexnum; ++v)
+		visited[v]=FALSE;	//初始化访问标记数组
+	time=0;
+	for(v=0; v<G.vexnum; ++v)	//从v=0开始遍历
+		if(!visited[v]) DFS(G, v);
+}
+
+void DFS(Graph G, int v){
+	visited[v]=TRUE;
+	visit(v);
+	for(w=FirstNeighbor(G, v); w>=0; w=NextNeighbor(G, v, w)
+		if(!visited[w]	{	//w为u的尚未访问的邻接顶点
+			DFS(G, w);
+			
+		}
+	time=time+1; finishTime[v]=time;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
