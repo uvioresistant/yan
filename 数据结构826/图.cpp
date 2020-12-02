acad27c5b3ff5ec417ee034659ffb42add0e1f31
uvioresistant <kaixin654321@gmail.com>
2020-12-02 16:44:57 +0800

    数据结构:添加排序_查找_来自数据结构(c++版)_20201202

---
+++ 数据结构826/图.cpp
@@ -0,0 +1,130 @@
+/*邻接矩阵法*/ 
+# define MaxVertexNum 100       //顶点数目的最大值 
+typedef struct{
+    char Vex[MaxVertexNum];     //顶点表~顶点中可以存放更复杂的信息 
+    int Edge[MaxVertexNum][MaxVertexNum];   //邻接矩阵，边表，可改用bool型或枚举型变量表示边 
+    int vexnum, arcnum;         //图的当前顶点数和边数/弧数 
+}MGraph;
+
+// 邻接矩阵法存储带权图(网)
+# define MaxVertexNum 100           //顶点数目的最大值 
+# define INFINITY 最大的int值       //宏定义常量"无穷"，可用int的上限值表示"无限" 
+typedef char VertexType;            //顶点的数据类型 
+typedef int EdgeType;               //带权图中，边上权值的数据类型 
+tepedef struct{
+    VertexType Vex[MaxVertexNum];   //顶点 
+    EdgeType Edge[MaxVertexNum][MaxVertexNum];  //边的权 
+    int vexnum, arcnum;             //图的当前顶点数和弧数 
+}MGraph;
+
+/*邻接表法(顺序+链式存储)*/
+//顶点
+typedef struct VNode{
+    VertexType data;    //顶点信息
+    ArcNode *first;     //第一条边/弧 
+}VNode, AdjList[MaxVertexNum];
+
+//边/弧
+typedef struct ArcNode{
+    int adjvex;             //边/弧指向哪个结点 
+    struct ArcNode *next;   //指向下一条弧的指针 
+    //InfoType info;        //边权值 
+}ArcNode; 
+
+//用邻接表存储的图
+typedef struct{
+    AdjList vertices;
+    int vexnum, arcnum;
+}AlGraph; 
+
+/*图的广度优先遍历*/
+bool visited[MAX_VERTEX_NUM]; //访问标记数组~初始都为false
+
+//广度优先遍历
+void BFS(Graph G, int v){	//从顶点v出发，广度优先遍历图G
+	visit(v);				//访问初始顶点v
+	visited[v]=TRUE;		//对v做已访问标记TRUE
+	Enqueue(Q, v);			//顶点v入队列Q
+	while(!isEmpty(Q)){
+		Dequeue(Q, v);		//顶点v出队列
+		for(w=FirstNeighbor(G, v); w>=0; w=NextNeighbor(G, v, w))
+			//检测v所有邻接点
+			if(!visited[w]){	//w为v的尚未访问的邻接顶点
+				visit(w);		//访问顶点w
+				visited[w]=TRUE;	//对w做已访问标记
+				EnQueue(Q, W);		//顶点w入队列
+			}
+	}
+}
+
+
+//BFS算法(Final版)
+bool visited[MAX_VERTEX_NUM]; //访问标记数组~初始都为false
+//广度优先遍历
+void BFSTraverse(Graph G){		//对图G进行广度优先遍历
+	for(i=0; i<G.vexnum; ++i)
+		visited[i]=FALSE;		//访问标记数组初始化
+	InitQueue(Q);				//初始化辅助队列Q
+	for(i=0; i<G.vexnum; ++i)	//从0号顶点开始遍历
+		if(!visited[i])			//对每个连通分量调用一次BFS
+			BFS(G, i);			//vi未访问过，从vi开始BFS
+}
+
+void BFS(Graph G, int v){	//从顶点v出发，广度优先遍历图G
+	visit(v);				//访问初始顶点v
+	visited[v]=TRUE;		//对v做已访问标记TRUE
+	Enqueue(Q, v);			//顶点v入队列Q
+	while(!isEmpty(Q)){
+		Dequeue(Q, v);		//顶点v出队列
+		for(w=FirstNeighbor(G, v); w>=0; w=NextNeighbor(G, v, w))
+			//检测v所有邻接点
+			if(!visited[w]){	//w为v的尚未访问的邻接顶点
+				visit(w);		//访问顶点w
+				visited[w]=TRUE;	//对w做已访问标记
+				EnQueue(Q, W);		//顶点w入队列
+			}
+	}
+}
+
+
+/*图的深度优先遍历*/
+bool visited[MAX_VERTEX_NUM];	//访问标记数组
+void DFS(Graph G, int v){		//从顶点v出发，深度优先遍历图G
+	visit(v);					//访问顶点v
+	visited[v]=TRUE;			//设已访问标记
+	for(w=FirstNeighbor(G, v);w>=0;w=NextNeighbor(G, v, w))
+		if(!visited[w]){		//w为u的尚未访问的邻接顶点
+			DFS(G, w);
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ 数据结构826/查找.cpp
@@ -0,0 +1,193 @@
+/*顺序表的顺序查找算法*/
+int SeqSearch1(int r[], int n, int k)
+{
+	r[0]=k;
+	i=n;
+	while(r[i]!=k)
+		i--;
+	return i;
+}
+
+/*单链表的顺序查找算法*/
+int SeqSearch2(Node<int> *first, int k)
+{
+	p=first->next; j=1;
+	while(p!=NULL&&p->data!=k)
+	{
+		p=p->next;
+		j++;
+	}
+	if(p->data==k) return j;
+		else return 0;
+}
+
+/*折半查找*/
+//有序表{7, 14, 18, 21, 23, 29, 31, 35, 38, 42, 46, 49, 52}中
+//1)查找关键码为14的记录
+//1)查找关键码为22的记录
+
+//折半查找非递归算法
+int BinSearch1(int r[], int n, int k)
+{
+	low=1; high=n;
+	while(low<=high)
+	{
+		mid=(low+high)/2
+		if(k<r[mid]) high=mid-1;
+			else if (k>r[mid]) low=mid+1;
+				else return mid;
+	}
+}
+
+//折半查找递归算法
+int BinSearch2(int r[], int low, int high, int k)
+{
+	if(low>high) return 0;	//递归的边界条件
+	else{
+		mid=(low+high)/2;
+		if(k<r[mid]) return BinSearch2(r, low, mid-1, k);
+			else if(k>r[mid]) return BinSearch2(r, mid+1, high, k);
+				else return mid;
+	}
+}
+
+/*二叉链表存储*/
+//给出对应查找集合{63, 55, 90, 42, 58, 70, 10, 45, 67, 83}的两个二叉排序树
+class BiSortTree	//假定只有一个整型数据项
+{
+public:
+	BiSortTree(int a[], int n);	//建立查找集合a[n]的二叉排序树
+		BiSortTree();			//析构函数，释放二叉排序树中所有结点，同二叉链表的析构函数
+	void InsertBST(BiNode<int> *root, BiNode<int> *s);	//在二叉排序树中插入结点s
+	void DeleteBST(BiNode<int> *f);						//删除结点f的左孩子结点p	
+	BiNode<int> * SearchBST(BiNode<int> *root, int k);	//查找值为k的结点
+private:
+	BiNode<int> * root;		//二叉排序树(即二叉链表)的根指针
+};
+
+//二叉排序树插入算法InsertBST
+void BiSortTree::InsertBST(BiNode<int> * root, BiNode<int> *s)
+{
+	if(root == NULL) root = s;
+		else if (s->data<root->data) InsertBST(root->lchild, s);
+			else InsertBST(root->rchild, s);
+}
+
+//二叉排序树构造函数算法BiSortTree
+BiSortTree::BiSortTree(int r[], int n)
+{
+		for(i=0; i<n; i++)
+		{
+			s=new BiNode<int>; s->data=r[i];
+			s->lchild=s->rchild=NULL;
+			InsertBST(root, s);
+		}
+}
+
+//二叉排序树的删除算法DeleteBST
+void BiSortTree::DeleteBST(BiNode<int> *p, BiNode<int> *f)
+{
+	if(!p->lchild && !p->rchild)	//p为叶子
+		f->lchild=NULL;
+		delete p;
+}
+else if(!p->rchild){		//p只有左子树
+	f->lchild=p->lchild;
+	delete p;
+}
+else if(!p->lchild){		//p只有右子树
+		f->lchild=p->rchild;
+		delete p;
+}
+else{
+	par=p; s=p->rchild;
+	while(s->lchild!=NULL)	//查找最左下结点
+	{
+		par=s;
+		s=s->lchild;
+	}
+	p->data=s->data;
+	if(par==p) par->rchild=s->rchild;	//处理特殊情况
+		else par->lchild=s->rchild;		//一般情况
+	delete s;
+}	//左右子树均不空的情况处理完毕
+
+//二叉排序树查找算法SearchBST
+BiNode * BiSortTree::SearchBST(BiNode<int> * root, int k)
+{
+	if (root == NULL) return NULL;
+		else if(root->data==k) return root;
+			else if (k<root->data) return SearchBST(root ->lchild, k);
+				else return SearchBST(root->rchild, k);
+}
+
+/*闭散列表的查找算法HashSearch1*/
+int HashSearch1(int ht[], int m, int k)
+{
+	j = H(k);
+	if(ht[j] == k) return j;	//没有发生冲突，比较一次查找成功
+	i=(j+1) % m;
+	while(ht[i]!=Empty && i!=j)
+	{
+		if(ht[i] == k) return i;	//发生冲突，比较若干次查找成功
+		i=(i+1) % m;	//向后探测一个位置
+	}
+	if(i == j) throw '溢出';
+		else ht[i] = k;		//查找不成功时插入
+	
+}
+
+/*开散列表的查找算法HashSearch2*/
+Node<int> * HashSearch2(Node<int> * ht[], int n, int k)
+{
+	j=H(k);
+	p=ht[j];
+	while(p && p->data!=k)
+		p=p->next;
+	if(p->data==k) return p;
+		else{
+			q=new Node<int>; q->data=k;
+			q->next=ht[j];
+			ht[j]=q;
+		}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
