ed534ba9c52f82b11e25bed709496cf0a99ed246
uvioresistant <kaixin654321@gmail.com>
2020-12-04 17:36:13 +0800

    数据结构_from_work：添加排序.cpp和查找导图

--- 数据结构826/排序.cpp
+++ 数据结构826/排序.cpp
@@ -10,3 +10,78 @@ void InsertSort(int r[], int n)
 	}
 	
 }
+
+/*希尔排序算法ShellSort*/
+void ShellSort(int r[], int n)
+{
+	for (d=n/2; d>=1; d=d/2)	//以增量为d进行直接插入排序
+	{
+		for (i=d+1; i<=n; i++)	//将r[i]插入到所属的子序列中
+		{
+			r[0]=r[i];	//暂存被插入记录,r[0]只是暂存单元，不是哨兵；
+			for(j=i-d; j>0 && r[0]<r[j]; j=j-d)	//插入记录r[i]时，自r[i-d]起，往前跳跃式(跳跃幅度为0)查找待插入位置，在查找过程中，记录后移也是跳跃d个位置
+				r[j+d]=r[j];		//记录后移d个位置，保证仍在同一个子序列 ,当搜索位置j<=0或r[0]>=r[j],表示插入位置已找到
+			r[j+d]=r[0];
+		}
+	}	
+}
+
+/*起泡排序算法BubbleSort*/
+void BubbleSort(int r[], int n)
+{
+	exchange=n;	//第一趟排序的范围是r[1]到r[n]
+	while(exchage)	//仅当上一趟排序有记录交换才进行本趟排序
+	{
+		bound=exchage; exchage=0;
+		for (j=1; j<bound; j++)	//一趟排序
+			if(r[j]>r[j+1]){
+				r[j]<-->r[j+1];
+				exchange=j;	//记录每一次发生记录交换的位置
+			}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
