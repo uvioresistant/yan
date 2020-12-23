531f35f0d71a64838ffb06908d4ff6f7e77c53c5
uvioresistant <kaixin654321@gmail.com>
2020-12-24 00:13:26 +0800

    添加数据结构:单链表补充头指针_头结点_常用操作

--- 数据结构826/from_web_answer.cpp
+++ 数据结构826/from_web_answer.cpp
@@ -70,7 +70,8 @@ NODE *createEnd(char arr[], int len)
     head->next = NULL;
     NODE *end = head;  // 尾指针初始化
 
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++)
+    {
 
         NODE *p = (NODE *)malloc(sizeof(NODE)); // 为每个数组元素建立一个节点
         p->data = arr[i];
@@ -89,7 +90,8 @@ void print(NODE *head)
     if (head == NULL) return;
 
     NODE *p = head->next;
-    while (p != NULL) {
+    while (p != NULL)
+    {
         printf("%c  ", p->data);
         p = p->next;
     }
@@ -189,7 +191,8 @@ NODE *delSame(NODE *head)
 
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
 
     char arr[] = "a1xbeffeaghbm";
     int len = (int)strlen(arr);
@@ -209,3 +212,248 @@ int main(int argc, const char * argv[]) {
 
     return 0;
 }
+/*2010.6*/
+void MiniValue (LinkedList A)
+
+//A是数据域为正整数且无序的单链表，本算法查找最小值结点且打印。若最小值结点的数值是奇数，则与后继结点值交换；否则，就删除其直接后继结点。
+
+{
+    p=A->next; //设A是头结点的头指针，p为工作指针。
+
+    pre=p; //pre指向最小值结点，初始假定首元结点值最小。
+
+    while(p->next!=null)//p->next是待比较的当前结点。
+
+    {
+        if(p->next->data < pre->data) pre=p->next;
+
+        p=p->next; //后移指针
+
+    }
+
+    printf(“最小值=%d\n”，pre->data);
+
+    if (pre->data%2!=0) //处理奇数
+
+            if(pre->next!=null) //若该结点没有后继，则不必交换
+
+    {
+        t= pre->data;    //交换完毕
+        pre->data=pre->next->data;
+        pre->next->data=t;
+    }
+
+    else//处理偶数情况
+
+        if(pre->next!=null)//若最小值结点是最后一个结点，则无后继
+
+        {
+            u=pre->next;    //释放后继结点空间
+            pre->next=u->next;
+            free(u);
+            }
+
+/*2015.17*/
+//5. 在任意位置插入数据
+bool Insert(ListNode* &pHead, int pos, int val){
+    int count = 0;    //一定要对其初始化
+    if (pos <= 0)
+        return 0;
+    else if (pHead == NULL){
+        if (pos == 1){
+            ListNode* cur = (ListNode*)malloc(sizeof(ListNode));
+            cur->val = val;
+            cur->next = NULL;
+            pHead = cur;
+        }
+        else
+            return false;
+    }
+    else{
+        ListNode* cur = pHead;
+        ListNode* pNode = pHead;
+        ListNode* temp = (ListNode*)malloc(sizeof(ListNode));
+        while (cur != NULL){
+            ++count;
+            if (pos == count){
+                if (pHead == cur){
+                    
+                    temp->next = pHead;
+                    pHead = temp;
+                    temp->val = val;
+                    break;
+                }
+                else{
+                    temp->next = cur;
+                    pNode->next = temp;
+                    temp->val = val;
+                    break;
+                }
+
+            }
+            
+            pNode = cur;
+            cur = cur->next;
+            
+        }
+        if (pos == count + 1)
+            pNode->next = newNode(val);            
+        else if (pos > count + 1){
+            free(temp);
+            return false;
+        }        
+    }
+    return true;
+}
+
+//6.头插
+pNode PushFront(pNode* pHead, DataType data)
+{
+	assert(pHead);
+	if (NULL == *pHead)
+	{
+		return NULL; 
+	}
+	else
+	{
+		pNode pnode = *pHead;
+		while (pnode->_pNext)
+		{
+			pnode = pnode->_pNext;
+		}
+		pnode->_pNext = BuyNode(data);
+	}
+	return;
+}
+
+
+//10.尾删
+pNode PopDel(pNode pHead)
+{
+	if (pHead == NULL)
+	{
+		return;
+	}
+	else if ((pHead)->_pNext == NULL)
+	{
+		free(pHead);
+		pHead = NULL;
+	}
+	else
+	{
+		pNode pTail = pHead;
+		pNode pPos = pTail;
+		while ( pTail->_pNext)
+		{
+			pPos = pTail;
+			pTail = pTail->_pNext;
+		}
+		free(pTail);
+		pTail = NULL;
+		pPos->_pNext = NULL;
+	}
+ 
+}
+
+/*2012.17*/
+/*    
+* Copyright (c) 2016, 烟台大学计算机与控制工程学院    
+* All rights reserved.    
+* 文件名称：LinkList.cpp    
+* 作    者：单昕昕    
+* 完成日期：2016年4月14日    
+* 版 本 号：v1.0    
+* 问题描述：有一个不带头节点的单链表，设计递归算法：
+            （1）求以h为头指针的单链表的节点个数
+            （2）反向显示以h为头指针的单链表的所有节点值
+* 程序输入：数组A[n]。   
+* 程序输出：n个元素的平均值。  
+*/      
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+//不带头节点的单链表的节点类型
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} LinkList;
+LinkList *h;
+void Create(LinkList *&h,int a[],int n)
+{
+    //建立一个不带头节点的单链表
+    int i;
+    LinkList *s,*r;
+    h=(LinkList *)malloc(sizeof(LinkList));
+    h->data=a[0];
+    h->next=NULL;
+    r=h;
+    for(i=1; i<n; ++i)
+    {
+        s=(LinkList *)malloc(sizeof(LinkList));
+        s->data=a[i];
+        r->next=s;
+        r=s;
+    }
+    r->next=NULL;
+}
+//求以h为头指针的单链表的节点个数
+int Number(LinkList *h)
+{
+    if(h==NULL)
+        return 0;
+    else
+        return 1+Number(h->next);
+}
+//反向显示以h为头指针的单链表的所有节点值
+void Display(LinkList *h)
+{
+    if(h==NULL)
+        return ;
+    else
+    {
+        Display(h->next);
+        cout<<h->data<<" ";
+        //如果是正向显示的话，cout写在Display递归上面
+    }
+}
+int main()
+{
+    int a[10]= {0,1,2,3,4,5,6,7,8,9};//作为预设单链表中的data值
+    Create(h,a,10);//尾插法将a数组中的值插入链表
+    cout<<"以h为头指针的单链表的节点个数=";
+    cout<<Number(h)<<endl;
+    cout<<"反向显示以h为头指针的单链表的所有节点值"<<endl;
+    Display(h);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
