b121b168107d7024029d4fb44ba8ef26e7923dad
uvioresistant <kaixin654321@gmail.com>
2021-12-01 10:10:30 +0800

    feat(树):添加树的BST和DST数据结构定义及实现

--- 数据结构826/树.cpp
+++ 数据结构826/树.cpp
@@ -502,6 +502,15 @@ typedef struct BSTNode{
     struct BSTNode *lchild, *rchild;
 }BSTNode, *BSTree;
 
+//二叉排序树的非递归查找算法(王道)
+BSTNode *BST_Search(BiTree T, ElemType key){
+	while(T!=NULL&&key!=T->data){	//若树空或等于根结点值，则结束循环
+		if(key<T->data)	T=T->lchild;	//小于，则在左子树上查找
+		else T=T->rchild;			// 大于，则在右子树上查找
+	}
+	return T;
+}
+
 //在二叉排序树中查找值为key的结点
 BSTNode *BST_Search(BSTree T, int key){
     while(T!=NULL&&key!=T->key){        //若树空或等于根结点值，则结束循环 
@@ -524,6 +533,23 @@ BSTNode *BSTSearch(BSTree T, int key){
 } 
 
 //插入
+//二叉排序树插入操作的算法(王道)
+int BST_Insert(BiTree &T, KeyType k){
+	if(T==NULL){			//原树为空，新插入的记录为根结点
+		T=(BiTree)malloc(sizeof(BSTNode));
+		T->key=k;
+		T->lchild=T->rchild=NULL;
+		return 1;			//返回1，插入成功
+	}
+	else if(k==T->key)		//树中存在相同关键字的结点，插入失败
+		return 0;
+	else if(k<T->key)		//插入到T的左子树
+		return BST_Insert(T->lchild,k);
+	else					//插入到T的右子树
+		return BST_Insert(T->rchild,k);
+}
+
+
 //在二叉排序树插入关键字为k的新结点(递归实现)~最坏空间复杂度O(h)
 int BST_Insert(BSTree &T, int k){
     if(T==NULL){        //原树为空，新插入的结点为根结点
@@ -541,6 +567,17 @@ int BST_Insert(BSTree &T, int k){
 } 
 
 //构造
+//构造二叉排序树的算法描述(王道)
+void Create_BST(BiTree &T, KeyType str[], int n){
+	T=NULL;			//初始时T为空树
+	int i=0;
+	while(i<n){		//依次将每个关键字插入到二叉排序树种
+		BST_Insert(T, str[i]);
+		i++;
+	}
+	
+}
+
 //按照str[]中的关键字序列建立二叉排序树
 void Create_BST(BSTree &T, int str[], int n){
     T=NULL;         //初始时T为空树
